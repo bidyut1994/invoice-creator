553de2076f934f05ac53e214b47bb0ffc8a20f45
bidyut1994 <bbboruah199@gmail.com>
2025-06-17 12:42:49 +0530

    add

--- src/app/page.js
+++ src/app/page.js
@@ -16,7 +16,7 @@ export default function Home() {
 
   return (
     <div className="flex fixed overflow-y-auto overflow-x-hidden">
-      <div className=" ">
+      <div className="  z-[50]">
         <Sidebar />
       </div>
       <div className="ml-[6vw] w-[40vw] h-screen  z-[10] border-r border-gray-200 fixed  top-0 ">
@@ -24,7 +24,7 @@ export default function Home() {
           <motion.div
             initial={{ opacity: 0, x: 100 }}
             animate={{ opacity: 1, x: 0 }}
-            exit={{ opacity: 0, x: -100 }}
+            exit={{ opacity: 0, x: 0 }}
           >
             <CompanyDetails />
           </motion.div>
@@ -33,15 +33,31 @@ export default function Home() {
           <motion.div
             initial={{ opacity: 0, x: 100 }}
             animate={{ opacity: 1, x: 0 }}
-            exit={{ opacity: 0, x: -100 }}
+            exit={{ opacity: 0, x: 0 }}
           >
             <Customer />
           </motion.div>
         )}
-        {activeTab === "product-details" && <ProductDetails />}
-        {activeTab === "payment-details" && <PaymentDetails />}
+        {activeTab === "product-details" && (
+          <motion.div
+            initial={{ opacity: 0, x: 100 }}
+            animate={{ opacity: 1, x: 0 }}
+            exit={{ opacity: 0, x: 0 }}
+          >
+            <ProductDetails />
+          </motion.div>
+        )}
+        {activeTab === "payment-details" && (
+          <motion.div
+            initial={{ opacity: 0, x: 100 }}
+            animate={{ opacity: 1, x: 0 }}
+            exit={{ opacity: 0, x: 0 }}
+          >
+            <PaymentDetails />
+          </motion.div>
+        )}
       </div>
-      <div className=" fixed right-0 top-0 min-w-[54vw]  bg-gray-200 ">
+      <div className=" fixed right-0 top-0 min-w-[54vw]  bg-gray-200 z-[50]">
         <DisplayInvoice />
       </div>
     </div>
